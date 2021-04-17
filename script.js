4fa878ef0cb0a8a25cdd54452bcc9b35ac5b1a05
Percebeirus <pedrocobobeltran@gmail.com>
2021-04-17 17:49:12 +0200

    Funciona calculadora

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function compute()//Calcula el valor del interés
 
     var year = new Date().getFullYear()+parseInt(years)//Calcula el año final
 
-    //if( check_principal() == true)   {  
-       // document.getElementById("result").innerHTML = 
-        //"If you deposit <mark>" + principal + "</mark>,<br>" +  
-        //"at an interest rate of <mark>" + rate + "%</mark>.<br>" + 
-        //"You will receive an amount of <mark>" + interest + "</mark>, <br>" + 
-        //"in the year <mark>" + year + "</mark><br>"
-//    }  
+    if( check_principal() == true)   {  
+       document.getElementById("result").innerHTML = 
+       "If you deposit <mark>" + principal + "</mark>,<br>" +  
+       "at an interest rate of <mark>" + rate + "%</mark>.<br>" + 
+       "You will receive an amount of <mark>" + interest + "</mark>, <br>" + 
+       "in the year <mark>" + year + "</mark><br>"
+    }  
 
 }
       
@@ -21,4 +21,16 @@ function updateRate()
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerHTML = rateval;
+}
+
+function check_principal(){//verifica si la cantidad es negativa
+    var p = document.getElementById("principal");//captura la cantidad inicial
+
+    if(p.value == 0 || p.value < 0){//revisa si la cantidad inicial es mayor que 0
+        alert("Enter a positive number");//warning
+        p.focus();//devuelve el foco a p
+        document.getElementById("result").innerHTML = "";//borra resultado
+        return false; //si falla es falso
+    }
+    return true;//si ok devuelve true
 }
